1f2fbb9a377ce77a9d6bd128a7f9df95a0b66afd
Lei <Crescent-Saturn@users.noreply.github.com>
2017-02-09 21:48:35 -0500

    mouse test

--- mouse_test.py
+++ mouse_test.py
@@ -1,19 +1,17 @@
 import cv2
-import numpy as np
+import numpy as np 
 
+def draw_circle(event,x,y,flags,param):
+	if event == cv2.EVENT_LBUTTONDOWN:
+	# events: L:left button down;up;DBLCLK:double click
+		cv2.circle(img,(x,y),10,(125,125,125),-1)
 
-def draw_circle(event, x, y, flags, param):
-    if event == cv2.EVENT_LBUTTONDOWN:
-        cv2.circle(img, (x, y), 10, (0, 255, 0), -1)
-
-
-img = np.zeros((512, 512, 3), np.uint8)
-cv2.namedWindow('Circle')
-cv2.setMouseCallback('Circle', draw_circle)
+img = np.zeros((512,512,3),np.uint8)
+cv2.namedWindow('image')
+cv2.setMouseCallback('image',draw_circle)
 
 while (1):
-    cv2.imshow('Circle', img)
-    if cv2.waitKey(20) == 27:
-        break
-
+	cv2.imshow('image',img)
+	if cv2.waitKey(20)&0xFF == 27:
+		break
 cv2.destroyAllWindows()
